9db9082d117182144476b8975814cbb7b9e7d375
QuocDes <dinhquoc161107@gmail.com>
2026-05-27 09:01:31 +0700

    Update danhba.py

--- danhba.py
+++ danhba.py
@@ -1,10 +1,10 @@
 class  LienHe:
     def __init__(self ,ten ,sdt ,email ):
-        ten.self = ten
-        sdt.self = sdt
-        email.self = email
+        self.ten = ten
+        self.sdt = sdt
+        self.email = email
     
-    # thêm liên lạc
+    # thêm liên lạc (Thế, Đạt)
     def themlienlac (danh_ba):
         ten = input("Nhap ten ")
         sdt = input("Nhap sdt")
@@ -12,7 +12,7 @@ class  LienHe:
         danh_ba.append(LienHe(ten,sdt,email))
         print("Da them lien lac")
     
-    #HAM HIEN THI
+    #HAM HIEN THI (Thế, Đạt)
     def hien_thi (danh_ba):
         if not danh_ba:
             print("Danh ba trong")
@@ -20,16 +20,16 @@ class  LienHe:
         for i, lh in enumerate(danh_ba):
             print(i, lh.ten)
 
-    #SAP XEP THEO TEN
+    #SAP XEP THEO TEN (Thế, Đạt)
     def sap_xep (danh_ba):
         danh_ba.sort (key=lambda lh: lh.ten.split()[-1].lower())
         print("Đã sắp xếp theo thứ tự")
 
-    #XOA LIEN HE
+    #XOA LIEN HE (An)
     def xoa_lien_he(danh_ba):
-    if not danh_ba:
-        print("Danh ba rong")
-        return
+        if not danh_ba:
+            print("Danh ba rong")
+            return
     tu_khoa = input("Nhap ten hoac sdt can xoa: ").lower()
     for lh in danh_ba:
         if tu_khoa == lh.ten.lower() or tu_khoa == lh.sdt:
@@ -38,7 +38,7 @@ class  LienHe:
             return
     print("Khong tim thay lien he")
 
-    #TIM KIEM THEO TEN HOAC SDT
+    #TIM KIEM THEO TEN HOAC SDT (An)
     def tim_kiem(danh_ba):
     if not danh_ba:
         print("Danh ba rong")
@@ -54,3 +54,76 @@ class  LienHe:
             tim_thay = True
     if not tim_thay:
         print("Khong tim thay lien he")
+        
+ # GHI FILE (Hải)
+    def ghi_file(danh_ba):
+        f = open("danhba.txt", "w", encoding="utf-8")
+
+        for lh in danh_ba:
+            f.write(lh.ten + "," + lh.sdt + "," + lh.email + "\n")
+
+        f.close()
+
+        print("Da ghi vao file")
+
+    # DOC FILE
+    def doc_file(danh_ba):
+        try:
+            f = open("danhba.txt", "r", encoding="utf-8")
+
+            for line in f:
+                ten, sdt, email = line.strip().split(",")
+
+                danh_ba.append(LienHe(ten, sdt, email))
+
+            f.close()
+
+        except:
+            print("Chua co file danh ba")
+
+
+
+# CHUONG TRINH CHINH (Hải)
+danh_ba = []
+
+# doc file khi mo chuong trinh (Hải)
+LienHe.doc_file(danh_ba)
+
+while True:
+
+    print("\n===== MENU =====")
+    print("1. Them lien lac")
+    print("2. Hien thi danh ba")
+    print("3. Tim kiem")
+    print("4. Xoa lien he")
+    print("5. Sap xep")
+    print("6. Luu vao file")
+    print("0. Thoat")
+
+    chon = input("Nhap lua chon: ")
+
+    if chon == "1":
+        LienHe.themlienlac(danh_ba)
+
+    elif chon == "2":
+        LienHe.hien_thi(danh_ba)
+
+    elif chon == "3":
+        LienHe.tim_kiem(danh_ba)
+
+    elif chon == "4":
+        LienHe.xoa_lien_he(danh_ba)
+
+    elif chon == "5":
+        LienHe.sap_xep(danh_ba)
+
+    elif chon == "6":
+        LienHe.ghi_file(danh_ba)
+
+    elif chon == "0":
+        LienHe.ghi_file(danh_ba)
+
+        break
+
+    else:
+        print("Lua chon khong hop le")
